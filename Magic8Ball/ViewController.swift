75579d1c7aea9e634c1d2e7637bef87b21cfb969
elizeurs <13ders@gmail.com>
2018-12-15 20:13:45 -0200

    6. Coding Challenge #2 - Swift Data Types, Variables and Arrays

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+  var randomBallIndex: Int = 0
+  
+  @IBOutlet weak var magicBallImageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
-
+  @IBAction func askBtnPressed(_ sender: UIButton) {
+    updateBallImages()
+  }
+  
+  func updateBallImages() {
+    
+    randomBallIndex = Int.random(in: 0 ... 4)
+    
+    magicBallImageView.image = UIImage(named: ballArray[randomBallIndex])
+  }
+  
 }
 
